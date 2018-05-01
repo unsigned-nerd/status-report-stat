82c164cfb829a2b4f66badf33244a82d464be39c
unsigned_nerd <unsigned_nerd@member.fsf.org>
2018-05-01 23:21:18 +0700

    I had better use undersocore for the top level script name to ease my coding experience as I am a newbie!

--- src/tests.py
+++ src/tests.py
@@ -9,7 +9,7 @@ class MyUnitTest(unittest.TestCase):
         """
         User runs the top level script from command-line like this:
         
-          $ python3 status-report-stat.py [STATUS_REPORT_FILE]
+          $ python3 status_report_stat.py [STATUS_REPORT_FILE]
         
         According to our style, the top level script will just delegate
         all the works to _run() and pass all the command-line arguments
@@ -17,7 +17,7 @@ class MyUnitTest(unittest.TestCase):
         """
 
         # sys.argv[0] is the script name
-        script_name = 'status-report-stat.py'
+        script_name = 'status_report_stat.py'
 
         # calling the script without an argument
         argv = (script_name, )
