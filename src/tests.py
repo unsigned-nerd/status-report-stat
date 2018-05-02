c1593ad6fd2921071e381f4782f0897bba137662
unsigned_nerd <unsigned_nerd@member.fsf.org>
2018-05-02 23:48:23 +0700

    no need to use test_canruntoplevelscript test case anymore as it is already tested in test_canshowhelpmsg test case.

--- src/tests.py
+++ src/tests.py
@@ -5,25 +5,6 @@ import status_report_stat
 
 class MyUnitTest(unittest.TestCase):
 
-    def test_canruntoplevelscript(self):
-        """
-        User runs the top level script from command-line like this:
-        
-          $ python3 status_report_stat.py [STATUS_REPORT_FILE]
-        
-        According to our style, the top level script will just delegate
-        all the works to _run() and pass all the command-line arguments
-        to it.
-        """
-
-        # sys.argv[0] is the script name
-        script_name = 'status_report_stat.py'
-
-        # calling the script without an argument
-        argv = (script_name, )
-
-        status_report_stat._run(argv)
-
     def test_canshowhelpmsg(self):
         """
         If user calls our script without any argument or incorrect
