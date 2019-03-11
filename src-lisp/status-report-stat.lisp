d4463b90a259d69cec464bf17adf378447694a52
unsigned_nerd <unsigned_nerd@member.fsf.org>
2019-03-12 01:41:25 +0700

    beautify the code by using macro

--- src-lisp/status-report-stat.lisp
+++ src-lisp/status-report-stat.lisp
@@ -1,13 +1,10 @@
-(defparameter *group-non-work* "non-work")
-(defparameter *group-non-computer-related-work* "non-computer-related work")
-(defparameter *group-project-management* "project-management")
-(defparameter *group-computer-related-work* "computer-related-work")
-
-(defmacro prompt-for-input (var prompt-message)
+(defmacro prompt-for-input (prompt-message &optional var)
     `(progn
-           (format t ,prompt-message)
-	       (finish-output)
-	       (setf ,var (read-line))))
+       (format t ,prompt-message)
+       (finish-output)
+       ,(if (null var)
+         `(read-line)
+	       `(setf ,var (read-line)))))
 
 (defmacro for-line (line in-stream &rest body)
   `(loop for ,line = (read-line ,in-stream nil 'eof)
@@ -15,9 +12,18 @@
      do
      	,@body))
 
-(defparameter *status-report-file-name* nil)
+(defmacro print-line (line)
+  `(format t "~A~%" ,line))
 
-(prompt-for-input *status-report-file-name* "Please enter the status-report file name: ")
-(with-open-file (in *status-report-file-name*)
-  (for-line line in
-      (format t "line: ~A~%" line)))
+; the program starts here
+
+; define constants
+(defparameter *group-non-work* "non-work")
+(defparameter *group-non-computer-related-work* "non-computer related work")
+(defparameter *group-project-management* "project management")
+(defparameter *group-computer-related-work* "computer-related work")
+
+(with-open-file (in-stream (prompt-for-input "Please enter the status-report file name: "))
+  (let (work-hours-table (make-hash-table))
+    (for-line line in-stream
+      (print-line line))))
